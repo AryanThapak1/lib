87b8a36f3d8e913bacb9ad3319008a85039597bc
ARYAN THAPAK <aryanthapak.8@gmail.com>
2024-03-31 01:02:54 +0530

    v1.0

--- utils/catchAysnc.js
+++ utils/catchAysnc.js
@@ -2,7 +2,7 @@ module.exports= catchAsync= fn => {
     return(req,res,next)=>{
       fn(req,res,next).catch(()=>{
         res.status(500).json({ error: "Internal Server Error" });
-        next(err)
+        next
       }
      )
     }
